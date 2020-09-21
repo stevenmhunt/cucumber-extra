b1816c705b0b71d3c589055fb538adf010557c0b
Steven Hunt <steven.mtw.hunt@gmail.com>
2020-09-20 20:10:41 -0400

    more changes

--- hooks/first.js
+++ hooks/first.js
@@ -1,8 +1,13 @@
-const { BeforeAll, Before, After, AfterAll } = require('cucumber');
+const { BeforeAll, Before, After, AfterAll, setDefaultTimeout } = require('cucumber');
+const { BeforeStep, AfterStep } = require('../hooks');
 const config = require('../src/config');
 
 const timeout = 1000;
 const tags = config.alwaysEnabled ? null : '@extra';
+
+if (config.timeouts.global) {
+    setDefaultTimeout(config.timeouts.global);
+}
 
 BeforeAll({ timeout }, () => {
     console.log('===== first before all');
@@ -19,3 +24,10 @@ After({ timeout, tags }, function hook({ pickle }) {
 AfterAll({ timeout }, () => {
     console.log('==== last after all');
 });
+
+if (config.steps.delay.before) {
+    BeforeStep(() => new Promise(r => setTimeout(r, config.steps.delay.before)));
+}
+if (config.steps.delay.after) {
+    AfterStep(() => new Promise(r => setTimeout(r, config.steps.delay.after)));
+}
--- src/args.js
+++ src/args.js
@@ -1,8 +1,42 @@
+const _ = require('lodash');
+
+const handlers = [];
+function addBeforeValueHandler(fn) {
+    handlers.push(fn);
+}
+
+function executeBeforeValueHandlers(value) {
+    let result = value;
+    handlers.forEach((fn) => {
+        result = fn(result);
+    });
+    return result;
+}
+
+const isTable = v => v.hashes && _.isFunction(v.hashes);
+
+function processValue(value) {
+    if (_.isFunction(value)) {
+        return value;
+    }
+    if (isTable(value)) {
+        value.rawTable = _.cloneDeep(value).rawTable.map(processValue);
+        return value;
+    }
+    if (_.isArray(value)) {
+        return value.map(processValue);        
+    }
+    if (_.isObject(value)) {
+        return _.mapValues(value, processValue);
+    }
+    return executeBeforeValueHandlers(value);
+}
 
 function processArguments(args) {
-    return args;
+    return processValue(args);
 }
 
 module.exports = {
-    processArguments
+    processArguments,
+    addBeforeValueHandler
 };
