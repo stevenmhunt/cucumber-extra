8ccf97e66c51c184abad274d755ea37ebf474af6
Steven Hunt <steven.mtw.hunt@gmail.com>
2020-09-20 20:14:50 -0400

    cleanup

--- hooks/first.js
+++ hooks/first.js
@@ -9,22 +9,6 @@ if (config.timeouts.global) {
     setDefaultTimeout(config.timeouts.global);
 }
 
-BeforeAll({ timeout }, () => {
-    console.log('===== first before all');
-});
-
-Before({ timeout, tags }, function hook({ pickle }) {
-    console.log('===== first before');
-});
-
-After({ timeout, tags }, function hook({ pickle }) {
-    console.log('===== last after');
-});
-
-AfterAll({ timeout }, () => {
-    console.log('==== last after all');
-});
-
 if (config.steps.delay.before) {
     BeforeStep(() => new Promise(r => setTimeout(r, config.steps.delay.before)));
 }
--- hooks/last.js
+++ hooks/last.js
@@ -6,16 +6,14 @@ const timeout = 1000;
 const tags = config.alwaysEnabled ? null : '@extra';
 
 function lastBeforeHook({ pickle }) {
-    console.log('===== last before');
     context.isRunning = true;
     context.pickle = pickle;
 }
 lastBeforeHook.__nostep = true; // prevents AfterStep hooks from running.
 Before({ timeout, tags }, lastBeforeHook);
 
-function firstAfterHook({ pickle }) {
+function firstAfterHook() {
     context.isRunning = false;
-    console.log('===== first after');
 }
 firstAfterHook.__nostep = true; // prevents BeforeStep hooks from running.
 After({ timeout, tags }, firstAfterHook);
